75b161269c2f09523ae30bfc16321208e4571fe8
MasterDoramu <qwerdc1234567890@gmail.com>
2021-07-03 18:12:35 +0300

    update model and migration Good: added new field

--- database/migrations/2021_07_03_145627_create_goods_table.php
+++ database/migrations/2021_07_03_145627_create_goods_table.php
@@ -15,6 +15,10 @@ class CreateGoodsTable extends Migration
     {
         Schema::create('goods', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->integer('price')->nullable();
+            $table->string('description')->nullable();
+            $table->string('picture')->nullable();
             $table->timestamps();
         });
     }
