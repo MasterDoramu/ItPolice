25f084bcb5066f2555db827d60b0e1e523a48725
MasterDoramu <qwerdc1234567890@gmail.com>
2021-07-03 22:32:58 +0300

    add model and migration Basket

---
+++ app/Models/Basket.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Basket extends Model
+{
+    use HasFactory;
+    protected $fillable = ['good_id'];
+
+    public function good()
+    {
+        return $this->hasMany('App\Good');
+    }
+}
---
+++ database/migrations/2021_07_03_192954_create_baskets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBasketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('baskets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('good_id');
+            $table->foreign('good_id')->references('id')->on('goods')->onCascade('update')->onDelete('cascade');
+            $table->timestamps();
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('baskets');
+    }
+}
+
