1a5c64fec834fe4aaa6f5c217b867e6b87423610
MasterDoramu <qwerdc1234567890@gmail.com>
2021-07-03 13:07:12 +0300

    add Navbar.vue

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+<div >
+
+    <nav class="navbar navbar-expand-lg navbar-dark  bg-dark ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Shop</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item" v-for="(item, index) in links" :key="index">
+                    <router-link class="nav-link " :to="item.href">{{item.title}}</router-link>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    title: "Main",
+                    href: "/"
+                },
+                {
+                    title: "Goods",
+                    href: "/goods"
+                },
+                {
+                    title: "Basket",
+                    href: "/basket"
+                },
+            ]
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+</style>
